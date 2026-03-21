0b1dd031c87ff10ee64633559f2c8935f703d1a2
Aditya-7-Yadav <anilyadav3241@gmail.com>
2026-03-22 01:30:28 +0530

    Adding path to create function

---
+++ src/main/java/com/aditya/proj1/prjcontroller/proj1controller_1.java
@@ -0,0 +1,36 @@
+package com.aditya.proj1.prjcontroller;
+import java.util.*;
+import com.aditya.proj1.entity.JournalEntry;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/Journ")
+public class proj1controller_1 {
+    private Map<Long,JournalEntry> list= new HashMap<>();
+    @GetMapping("/viewAll")
+    public List<JournalEntry> getList() {
+        return new ArrayList<>(list.values());
+    }
+
+    @PostMapping("/insert")
+    public boolean create(@RequestBody JournalEntry newentry){
+        list.put(newentry.getId(), newentry);
+        return true;
+    }
+    @GetMapping("/see/{CurrId}")
+    public JournalEntry show(@PathVariable Long CurrId){
+        return list.get(CurrId);
+    }
+
+    @PutMapping("/update/{currId}")
+    public boolean update(@PathVariable Long currId, @RequestBody JournalEntry curr){
+        list.put(currId,curr);
+        return true;
+    }
+
+    @DeleteMapping("/delete/{CurrId}")
+    public boolean delete(@PathVariable Long CurrId){
+        list.remove(CurrId);
+        return true;
+    }
+}
